23294e14c29123c40b79a35d7a6ce3b5b0532c83
Billy Mott <92488759+Billygm@users.noreply.github.com>
2023-01-22 12:22:33 -0800

    editing contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,49 +12,50 @@ const styles = {
 };
 const Contact = () => {
   return (
-    <form style={styles.primary} class="m-3">
-      <fieldset>
-        <legend>Contact Me</legend>
-        <div class="mb-3">
-          <label for="nameInput" class="form-label">
-            Name
-          </label>
-          <input
-            type="text"
-            id="nameInput"
-            class="form-control"
-            placeholder="Name"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="emailInput" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="emailInput"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="messageInput" class="form-label">
-            Message
-          </label>
-          <textarea
-            type="text"
-            id="messageInput"
-            class="form-control"
-            rows="3"
-            placeholder="Message"
-          />
-        </div>
-        <button type="submit" class="btn btn-primary">
-          Submit
-        </button>
-        <p>Form currently not working; to contact me send an email to <a href="mailto:Billygm@uw.edu">Billygm@uw.edu</a></p>
-      </fieldset>
-    </form>
+    <p>Please email me at Billy.g.m@gmail.com</p>
+    // <form style={styles.primary} class="m-3">
+    //   <fieldset>
+    //     <legend>Contact Me</legend>
+    //     <div class="mb-3">
+    //       <label for="nameInput" class="form-label">
+    //         Name
+    //       </label>
+    //       <input
+    //         type="text"
+    //         id="nameInput"
+    //         class="form-control"
+    //         placeholder="Name"
+    //       />
+    //     </div>
+    //     <div class="mb-3">
+    //       <label for="emailInput" class="form-label">
+    //         Email address
+    //       </label>
+    //       <input
+    //         type="email"
+    //         class="form-control"
+    //         id="emailInput"
+    //         aria-describedby="emailHelp"
+    //       />
+    //     </div>
+    //     <div class="mb-3">
+    //       <label for="messageInput" class="form-label">
+    //         Message
+    //       </label>
+    //       <textarea
+    //         type="text"
+    //         id="messageInput"
+    //         class="form-control"
+    //         rows="3"
+    //         placeholder="Message"
+    //       />
+    //     </div>
+    //     <button type="submit" class="btn btn-primary">
+    //       Submit
+    //     </button>
+    //     <p>Form currently not working; to contact me send an email to <a href="mailto:Billygm@uw.edu">Billygm@uw.edu</a></p>
+    //   </fieldset>
+    // </form>
   );
 };
 
